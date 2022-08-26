8321919e123e9ae8a9f832cf9683136c8d59a479
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2022-08-26 23:04:39 +0900

    design: body(margin-top, box-shadow), a(text-decoration)

--- src/globalstyle.ts
+++ src/globalstyle.ts
@@ -49,10 +49,17 @@ export const GlobalStyle = createGlobalStyle`
   ::-webkit-scrollbar{
     display:none;
   }
+
+  a{
+    text-decoration:none;
+    color: black;
+  }
+
   body{
     max-width:390px;
-    border: 1px solid black;
     margin: 0 auto;
+    margin-top: 30px;
+    box-shadow: 3px 3px 3px 3px black
   }
   
   
