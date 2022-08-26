dac05f4c52f6f1b1366456dedf82a3395ce434e7
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2022-08-26 14:04:25 +0900

    feat: 네비게이션 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navigation from "./components/Navigation";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,5 @@
+function Navigation() {
+  return <div>네이게이션</div>;
+}
+
+export default Navigation;
