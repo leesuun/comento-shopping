86fddf0df1127e6ae9b6cdd486f51648aab235bb
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2022-08-26 14:31:07 +0900

    feat: 쇼핑몰 컴포넌트 추가 및 router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Basket from "./components/Basket";
+import Home from "./components/Home";
+import ProductDetail from "./components/ProductDetail";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/product/:productId" element={<ProductDetail />}></Route>
+        <Route path="/basket" element={<Basket />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
