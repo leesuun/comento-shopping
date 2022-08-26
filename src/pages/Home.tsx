19ed21972954e4c1be52d04443776255b1d7d1f8
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2022-08-26 18:43:27 +0900

    add: test용 api데이터 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { cupItems, winterItems } from "../apis/productItem";
 
 import ProductItem from "../components/ProductItem";
 import ThemeBtn from "../components/ThemeBtn";
@@ -19,9 +20,7 @@ export const ProductSection = styled.div`
   padding: 40px;
 `;
 
-export const ProductList = styled.ul`
-  /* border: 1px solid red; */
-`;
+export const ProductList = styled.ul``;
 
 function Home() {
   return (
@@ -33,21 +32,13 @@ function Home() {
       <GrayLine />
       <ProductSection>
         <ProductList>
-          <ProductItem
-            name="비숑 블랙 머그잔"
-            description="쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다."
-            thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg"
-          />
-          <ProductItem
-            name="가열 보온 티코스터 온열 원터치 컵 받침대"
-            description="언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다."
-            thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product2.jpg"
-          />
-          <ProductItem
-            name="벨루즈까사 솜사탕 파스텔 머그 4종 세트"
-            description="솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그."
-            thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg"
-          />
+          {cupItems.map((item) => (
+            <ProductItem
+              name={item.name}
+              description={item.description}
+              thumbnail={item.thumbnail}
+            />
+          ))}
         </ProductList>
       </ProductSection>
     </div>
