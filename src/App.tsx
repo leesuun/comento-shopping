817276b70148137385ac59597e1b446b4afa990b
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2022-08-27 17:53:24 +0900

    develop 브랜치 변경사항 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import { GlobalStyle } from "./globalstyle";
 import Basket from "./pages/Basket";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
-    <BrowserRouter basename="/comento-shopping">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/product/:productId" element={<ProductDetail />}></Route>
-        <Route path="/basket" element={<Basket />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter basename="/comento-shopping">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/product/:productId" element={<ProductDetail />}></Route>
+          <Route path="/basket" element={<Basket />}></Route>
+        </Routes>
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   );
 }
 
