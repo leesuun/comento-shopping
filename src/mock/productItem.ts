f27360d42eeff890244512f7042c4ce701f212e1
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2022-09-01 17:17:20 +0900

    feat: 제품 mock데이터 interface 수정 및 id정보 추가

--- src/mock/productItem.ts
+++ src/mock/productItem.ts
@@ -3,6 +3,7 @@ export interface ProductInfo {
   description?: string;
   thumbnail: string;
   price: number;
+  id: number;
 }
 
 export interface productItemsType {
@@ -15,6 +16,7 @@ export const productItems: productItemsType = {
   cup: {
     items: [
       {
+        id: 1000,
         name: "비숑 블랙 머그잔",
         description:
           "쌀쌀한 날씨에 따뜻한 우유, 커피 한잔하기 좋은 블랙 & 화이트 비숑 머그잔입니다.",
@@ -23,6 +25,7 @@ export const productItems: productItemsType = {
         price: 20000,
       },
       {
+        id: 1001,
         name: "가열 보온 티코스터 온열 원터치 컵 받침대",
         description:
           "언제나 따뜻하게 최대 12시간 동안 최대 60도의 온도로 따듯한 차를 즐길 수 있습니다.",
@@ -31,6 +34,7 @@ export const productItems: productItemsType = {
         price: 15000,
       },
       {
+        id: 1002,
         name: "벨루즈까사 솜사탕 파스텔 머그 4종 세트",
         description:
           "솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그.",
@@ -43,6 +47,7 @@ export const productItems: productItemsType = {
   winter: {
     items: [
       {
+        id: 2000,
         name: "해피데이 따뜻해 포켓용 핫팩 100g",
         description:
           "다른 제품에 비해 열이 훨씬많이나며, 주머니에 넣어두면 10분 정도 후 자연히 열이 발생합니다.",
@@ -51,6 +56,7 @@ export const productItems: productItemsType = {
         price: 5000,
       },
       {
+        id: 2001,
         name: "겟룩 겨울 방한 장갑",
         description:
           "내구성이 높은 고밀도 원단을 사용하여 날카로운 사물에 의해 쉽게 찢어지지 않습니다.",
@@ -59,6 +65,7 @@ export const productItems: productItemsType = {
         price: 25000,
       },
       {
+        id: 2002,
         name: "발렌티노크리스티 스포츠양말 중목 6켤레",
         description:
           "흘러내림 방지를 위해 발목 전체 탄력있는 서포트 밴딩처리로 오랜시간 안정감있는 착용 가능!",
