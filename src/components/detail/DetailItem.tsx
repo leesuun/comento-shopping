ba3170544bcdc0a51b52a7fdf6c4b0e54fcec250
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2022-09-01 17:00:22 +0900

    design: 제품 디테일 페이지의 DetailItem 구조 구성 및 디자인

--- src/components/detail/DetailItem.tsx
+++ src/components/detail/DetailItem.tsx
@@ -1,13 +1,29 @@
 import styled from "styled-components";
 
 const Item = styled.div``;
-const Img = styled.img``;
-const Info = styled.div``;
-const Name = styled.div``;
+const Img = styled.img`
+  width: 390px;
+  height: 420px;
+`;
+const Info = styled.div`
+  padding: 20px;
+`;
+const Name = styled.div`
+  margin-bottom: 7px;
+  font-weight: bold;
+`;
 const Price = styled.div``;
 
 function DetailItem() {
-  return null;
+  return (
+    <Item>
+      <Img src="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product1.jpg" />
+      <Info>
+        <Name>해피데이 따뜻해 포켓용 핫팩 100g</Name>
+        <Price>10000원</Price>
+      </Info>
+    </Item>
+  );
 }
 
 export default DetailItem;
