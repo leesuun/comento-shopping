10e308689671e1447a1477d2586e3ba59642393c
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2022-09-01 16:51:06 +0900

    feat: 디테일 컴포넌트 제작

---
+++ src/components/detail/DetailItem.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Item = styled.div``;
+const Img = styled.img``;
+const Info = styled.div``;
+const Name = styled.div``;
+const Price = styled.div``;
+
+function DetailItem() {
+  return null;
+}
+
+export default DetailItem;
