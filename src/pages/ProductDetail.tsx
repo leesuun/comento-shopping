8af9816317a81a42a2cbf89a54821a06325a72c8
SuUn Lee <77526673+leesuun@users.noreply.github.com>
2022-09-06 20:12:57 +0900

    feat: 디테일 페이지 버튼 상태 구현 및 clicked된 버튼에 따른 컴포넌트 노출

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import DetailItem from "../components/detail/DetailItem";
@@ -12,8 +13,9 @@ const Button = styled.button`
   border: none;
 `;
 
-const ProductExplain = styled.img``;
-const ProductComment = styled.div``;
+const Explain = styled.div``;
+const CommentGroup = styled.ul``;
+const Comment = styled.li``;
 export const BasketBtn = styled.button`
   width: 100%;
   height: 70px;
@@ -25,16 +27,27 @@ export const BasketBtn = styled.button`
 function ProductDetail() {
   const location = useLocation();
   const { id } = location.state as IProductDetailProps;
+  const [isComment, setIsComment] = useState(false);
+
+  const onClickBtn = () => setIsComment((prev) => !prev);
 
   return (
     <div>
       <DetailItem id={id} />
       <ButtonGroup>
-        <Button>상품 설명</Button>
-        <Button>상품 후기</Button>
+        <Button onClick={onClickBtn}>상품 설명</Button>
+        <Button onClick={onClickBtn}>상품 후기</Button>
       </ButtonGroup>
-      <ProductExplain />
-      <ProductComment>코멘트</ProductComment>
+      {isComment ? (
+        <CommentGroup>
+          <Comment key={1}>코멘트</Comment>
+          <Comment key={2}>코멘트</Comment>
+          <Comment key={3}>코멘트</Comment>
+        </CommentGroup>
+      ) : (
+        <Explain>상품 설명</Explain>
+      )}
+
       <Link to={{ pathname: "/basket" }}>
         <BasketBtn>장바구니 담기</BasketBtn>
       </Link>
